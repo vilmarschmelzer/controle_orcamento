c94cf523070ac848f56854e5affa669ce224dc7c
Vilmar Schmelzer <vilmarsss@gmail.com>
2014-07-30 12:56:49 -0300

    Este arquivo não tem utilidade

--- controle/controle/context_processors.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-
-def global_settings(request):
-    # return any necessary values
-    return {
-    'PERM_GRUPO_ADM': settings.PERM_GRUPO_ADM,
-    'PERM_GRUPO_VENDEDOR': settings.PERM_GRUPO_VENDEDOR
-    }
