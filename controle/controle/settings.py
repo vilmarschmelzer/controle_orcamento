6e87fbbdd2fee20aa5cb13a0a6366a59eae35d60
Vilmar Schmelzer <vilmarsss@gmail.com>
2014-07-30 13:59:42 -0300

    Atualizado configurações da aplicação

--- controle/controle/settings.py
+++ controle/controle/settings.py
@@ -26,6 +26,9 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#default 'django.contrib.sessions.serializers.JSONSerializer'
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+
 
 # Application definition
 
@@ -37,7 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
-    'orcamento'
+    'app_orcamento'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,7 +82,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
 
 
 # Static files (CSS, JavaScript, Images)
@@ -96,5 +100,35 @@ TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), '../templates')
 )
 
-PERM_GRUPO_ADMINISTRADOR = 'Administrador'
-PERM_GRUPO_VENDEDOR = 'Vendedor'
+PERM_GRUPO_ADMINISTRADOR = 1
+PERM_GRUPO_VENDEDOR = 2
+NR_REGISTROS_PAGINA = 8
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
