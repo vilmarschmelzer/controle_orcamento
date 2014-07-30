4dcb6a3feadbe81f7f722ff6afd931f33be10a1e
Vilmar Schmelzer <vilmarsss@gmail.com>
2014-07-30 12:41:45 -0300

    esta pasta foi deletada, devido ter sido renomeda para app_orcamento

--- controle/orcamento/models/cliente.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Cliente(models.Model):
-    documento = models.CharField(max_length=14)
-    nome = models.CharField(max_length=200)
-
-    class Meta:
-        app_label = 'orcamento'
--- controle/orcamento/models/produto.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Produto(models.Model):
-    nome = models.CharField(max_length=250)
-    descricao = models.CharField(max_length=250,null=True)
-    valor = models.FloatField()
-
-    class Meta:
-        app_label = 'orcamento'
